2a49cb894392bd82750b29c60b88875a6527dbaf
Max <75096038+AltronMaxX@users.noreply.github.com>
2026-01-13 23:42:08 +0400

    some things from dreamcast port

--- src/core/re3.cpp
+++ src/core/re3.cpp
@@ -1293,8 +1293,24 @@ void re3_assert(const char *expr, const char *filename, unsigned int lineno, con
 	abort();
 #else
 	// TODO
-	printf("\nREVC ASSERT FAILED\n\tFile: %s\n\tLine: %d\n\tFunction: %s\n\tExpression: %s\n",filename,lineno,func,expr);
-	assert(false);
+	fflush(stdout);
+	fflush(stderr);
+	dbglog(DBG_CRITICAL, "\nRE3 ASSERT FAILED\n\tFile: %s\n\tLine: %d\n\tFunction: %s\n\tExpression: %s\n", filename, lineno, func, expr);
+	dbglog(DBG_CRITICAL, "POSIX error (may not be relevant): %s\n", strerror(errno));
+#if defined(DC_SIM) || defined(DC_TEXCONV)
+	for(;;)
+		;
+#else
+	stacktrace();
+	dbgio_dev_select("fb");
+	sleep(1);
+	dbglog(DBG_CRITICAL, "RE3 ASSERT FAILED\n\tFile: %s\n\tLine: %d\n\tFunction: %s\n\tExpression: %s\n", filename, lineno, func, expr);
+	dbglog(DBG_CRITICAL, "POSIX error (may not be relevant): %s\n", strerror(errno));
+	stacktrace();
+	dbgio_flush();
+	abort();
+#endif
+
 #endif
 }
 #endif
