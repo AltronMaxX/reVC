80a800e129694363b082b252efa2873c3287a822
Max <75096038+AltronMaxX@users.noreply.github.com>
2026-06-29 03:39:01 +0400

    fix flatpak discord
    И как он у меня раньше работал?

--- vendor/discord-rpc/src/connection_unix.cpp
+++ vendor/discord-rpc/src/connection_unix.cpp
@@ -63,6 +63,7 @@ bool BaseConnection::Open()
     setsockopt(self->sock, SOL_SOCKET, SO_NOSIGPIPE, &optval, sizeof(optval));
 #endif
 
+    // Standard paths: $TEMP/discord-ipc-0 .. 9
     for (int pipeNum = 0; pipeNum < 10; ++pipeNum) {
         snprintf(
           PipeAddr.sun_path, sizeof(PipeAddr.sun_path), "%s/discord-ipc-%d", tempPath, pipeNum);
@@ -72,6 +73,21 @@ bool BaseConnection::Open()
             return true;
         }
     }
+
+    // Flatpak Discord: $TEMP/app/com.discordapp.Discord/discord-ipc-0 .. 9
+    for (int pipeNum = 0; pipeNum < 10; ++pipeNum) {
+        snprintf(PipeAddr.sun_path,
+          sizeof(PipeAddr.sun_path),
+          "%s/app/com.discordapp.Discord/discord-ipc-%d",
+          tempPath,
+          pipeNum);
+        int err = connect(self->sock, (const sockaddr*)&PipeAddr, sizeof(PipeAddr));
+        if (err == 0) {
+            self->isOpen = true;
+            return true;
+        }
+    }
+
     self->Close();
     return false;
 }
